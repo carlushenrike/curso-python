3db11567c3a4dde7bc441ad921485e8373acf291
carlushenrike <carlushenrike222@gmail.com>
2023-01-18 11:33:37 -0300

    aula05

---
+++ aulas/formatters.py
@@ -0,0 +1,18 @@
+"""
+Aula 05
+s - str
+d - int
+f - float ( pontos fluantes) :.2f
+. - (casas decimais)
+
+"""
+
+n1 = 30
+n2 = 2
+divisao = n1 / n2
+print(f'{divisao:.2f}')
+
+nome = 'Carlos Henrique Addams'
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
