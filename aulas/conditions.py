19609b924c9b1b360654be46e9ef00553996cf20
carlushenrike <carlushenrike222@gmail.com>
2023-01-18 10:29:03 -0300

    aula04

--- aulas/conditions.py
+++ aulas/conditions.py
@@ -10,3 +10,38 @@ else:
 
 if idade > maioridade or idade < menoridade:
     print('Não pode pagar empréstimo')
+
+"""
+and, or, not
+not in, in
+"""
+
+nome = "Carlos Henrique"
+if "Car" in nome:
+    print('existe')
+else:
+    print('não existe')
+
+a = 0
+if not a:
+    print('Preencha valor de A')
+else:
+    print('a')
+
+b = 2
+if not b < a:
+    print('B maior que A')
+else:
+    print('A maior que B')
+
+
+usuario = input('Usuário:')
+senha = input('Senha:')
+print(type(senha))
+usuarioms = 'Carlos'
+senhams = "bD'h2x54"
+
+if usuario == usuarioms and senha == senhams:
+    print('Connected')
+else:
+    print('Invalid Error')
